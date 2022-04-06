09ac2f37f2f4a3206302a4515669e7db4340ac17
slmaaa <maszelong2001@gmail.com>
2022-04-06 23:48:13 +0800

    update

--- login.js
+++ login.js
@@ -12,38 +12,47 @@ import {
   getAuth,
   GoogleAuthProvider,
   signInWithRedirect,
-  getRedirectResult,
 } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
 
 export const Login = ({ setCurrentPage, app }) => {
   const auth = useRef(null);
+  const user = useRef(null);
+
   const [isLoading, setIsLoading] = useState(true);
   const [isLogined, setIsLogined] = useState(false);
-
   useEffect(() => {
     auth.current = getAuth(app.current);
+    setIsLoading(false);
+    console.log(isLogined);
   }, []);
 
-  return isLoading
-    ? html`<p>Loading</p>`
-    : html`
-        <div
-          class="hero px-5 py-5 is-flex is-flex-direction-column is-fullheight"
-        >
-          <div class="pb-5">
-            <h1
-              class="title is-3 has-text-primary is-primary has-text-weight-bold"
-            >
-              Greenhub
-            </h1>
-            <h1 class="subtitle has-text-dark has-text-weight-light">
-              Login to your account
-            </h1>
-          </div>
-        </div>
-      `;
+  useEffect(() => {
+    if (!isLogined) return;
+  }),
+    [isLogined];
+
+  const handleGoogleLogin = () => {
+    signInWithRedirect(auth.current, provider);
+    setIsLogined(true);
+  };
+
+  return html`
+    <div class="hero px-5 py-5 is-flex is-flex-direction-column is-fullheight">
+      <div class="pb-5">
+        <h1 class="title is-3 has-text-primary is-primary has-text-weight-bold">
+          Greenhub
+        </h1>
+        <h1 class="subtitle has-text-dark has-text-weight-light">
+          Login to your account
+        </h1>
+        <button class="button is-primary" onClick=${handleGoogleLogin}>
+          Google
+        </button>
+      </div>
+    </div>
+  `;
 };
 
 export default Login;
--- main.js
+++ main.js
@@ -10,13 +10,18 @@ import htm from "https://cdn.skypack.dev/htm";
 const html = htm.bind(h);
 
 import Login from "./login.js";
+import Home from "./home.js";
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import {
+  getAuth,
+  GoogleAuthProvider,
+  getRedirectResult,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCKyQWZ6hsQ6xyIMZGsriZgoZlGTU1RgTQ",
   authDomain: "greenhub-9c7d3.firebaseapp.com",
@@ -28,24 +33,55 @@ const firebaseConfig = {
 
 const Main = () => {
   //useStates for routing
-  const app = useRef(null);
+  const app = useRef(initializeApp(firebaseConfig));
+  const user = useRef(null);
   const [currentPage, setCurrentPage] = useState("LOGIN");
+  const [isLoading, setIsLoading] = useState(true);
 
   const _URL = new URL(document.location);
   const _GET = _URL.searchParams;
   const URL_TARGET = _GET.get("id");
+
   useEffect(() => {
-    //Intialize Firebase
-    app.current = initializeApp(firebaseConfig);
+    const auth = getAuth(app.current);
+    getRedirectResult(auth)
+      .then((result) => {
+        console.log(result);
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        user.current = result.user;
+        // The signed-in user info.
+        setCurrentPage("HOME");
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   useEffect(() => {
     console.log(currentPage);
   }, [currentPage]);
 
+  if (isLoading) {
+    return html`<p>Loading...</p>`;
+  }
+
   switch (currentPage) {
     case "LOGIN":
       return html`<${Login} setCurrentPage=${setCurrentPage} app=${app} />`;
+    case "HOME":
+      return html`<${Home} setCurrentPage=${setCurrentPage} user=${user} />`;
   }
 };
 
