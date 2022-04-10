29f98ea87237f8b67e4a9ece3a1b5763cf0d4b8b
slmaaa <maszelong2001@gmail.com>
2022-04-10 17:18:18 +0800

    update

--- search.js
+++ search.js
@@ -3,84 +3,84 @@ import { h } from "https://cdn.skypack.dev/preact";
 import htm from "https://cdn.skypack.dev/htm";
 const html = htm.bind(h);
 import {
-  useEffect,
-  useState,
-  useRef,
+    useEffect,
+    useState,
+    useRef,
 } from "https://cdn.skypack.dev/preact/hooks";
 
 import NavBar from "./navigation_bar.js";
 
 var googleMapSrc =
-  "https://maps.googleapis.com/maps/api/js?key=AIzaSyC3vtiKXk5oOqyFRxIGiWd41XMe5gAKbUE";
+    "https://maps.googleapis.com/maps/api/js?key=AIzaSyC3vtiKXk5oOqyFRxIGiWd41XMe5gAKbUE";
 
 const Search = ({ setCurrentPage, currentPage }) => {
-  const [inputDestination, setInputDestination] = useState("");
-  const [isInputDestinationFocused, setIsInputDestinationFocused] =
+    const [inputDestination, setInputDestination] = useState("");
+    const [isInputDestinationFocused, setIsInputDestinationFocused] =
     useState(false);
-  const [dropdownItems, setDropdownItems] = useState([]);
+    const [dropdownItems, setDropdownItems] = useState([]);
 
-  function loadScript(sScriptSrc, loadedCallback) {
-    var oHead = document.getElementsByTagName("HEAD")[0];
-    var oScript = document.createElement("script");
-    oScript.type = "text/javascript";
-    oScript.src = sScriptSrc;
-    oHead.appendChild(oScript);
-    oScript.onload = loadedCallback;
-  }
-
-  // Initialize and add the map
-  function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    const infoWindow = new google.maps.InfoWindow();
-
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 19,
-      center: uluru,
-      mapTypeControl: false,
-      streetViewControl: false,
-      fullscreenControl: false,
-    });
-    // The marker, positioned at Uluru
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+    function loadScript(sScriptSrc, loadedCallback) {
+        var oHead = document.getElementsByTagName("HEAD")[0];
+        var oScript = document.createElement("script");
+        oScript.type = "text/javascript";
+        oScript.src = sScriptSrc;
+        oHead.appendChild(oScript);
+        oScript.onload = loadedCallback;
     }
-  }
 
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-      browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
-  }
+    // Initialize and add the map
+    function initMap() {
+        // The location of Uluru
+        const hong_kong = { lat: 22.3526632, lng: 113.8475072 };
+        // The map, centered at Uluru
+        const infoWindow = new google.maps.InfoWindow();
 
-  useEffect(() => {
-    loadScript(googleMapSrc, initMap);
-  }, []);
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 17,
+            center: hong_kong,
+            mapTypeControl: false,
+            streetViewControl: false,
+            fullscreenControl: false,
+        });
+        // The marker, positioned at Uluru
 
-  return html`
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(
+            browserHasGeolocation ?
+            "Error: The Geolocation service failed." :
+            "Error: Your browser doesn't support geolocation."
+        );
+        infoWindow.open(map);
+    }
+
+    useEffect(() => {
+        loadScript(googleMapSrc, initMap);
+    }, []);
+
+    return html `
     <div
       id="search-bar"
-      class="field my-5 is-overlay search-bar has-addons-centered is-flex is-align-items-center is-justify-content-center"
+      class="field my-5 mx-3 is-overlay search-bar has-addons-centered is-flex is-align-items-center is-justify-content-center"
     >
       <div
         class="dropdown ${inputDestination.length <= 1 ||
