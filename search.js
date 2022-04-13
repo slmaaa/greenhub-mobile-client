4639a3b64514b206448e6be8c87b3b94b5a06402
slmaaa <maszelong2001@gmail.com>
2022-04-14 00:22:19 +0800

    update and test fetch

--- search.js
+++ search.js
@@ -20,92 +20,99 @@ const SEARCH_RADIUS = "1000";
 const RESTAURANT_REST_URL = "https://greenhub.slmaaa.work/backend/restaurant/";
 
 const Search = ({ setCurrentPage, currentPage }) => {
-    const [searchInput, setSearchInput] = useState("");
-    const [isInputSearchFocused, setIsInputSearchFocused] = useState(false);
-    const [dropdownItems, setDropdownItems] = useState([]);
+        const [searchInput, setSearchInput] = useState("");
+        const [isInputSearchFocused, setIsInputSearchFocused] = useState(false);
+        const [dropdownItems, setDropdownItems] = useState([]);
 
-    const loadScript = (sScriptSrc, loadedCallback) => {
-        var oHead = document.getElementsByTagName("HEAD")[0];
-        var oScript = document.createElement("script");
-        oScript.type = "text/javascript";
-        oScript.src = sScriptSrc;
-        oHead.appendChild(oScript);
-        oScript.onload = loadedCallback;
-    };
+        const mapRef = useRef(null);
 
-    const getNearbyRestaurants = async(lat, lng) => {
-        getData(
-                RESTAURANT_REST_URL +
-                "?" +
-                new URLSearchParams({
-                    lat: lat,
-                    lng: lng,
-                    dst: SEARCH_RADIUS,
+        const loadScript = (sScriptSrc, loadedCallback) => {
+            var oHead = document.getElementsByTagName("HEAD")[0];
+            var oScript = document.createElement("script");
+            oScript.type = "text/javascript";
+            oScript.src = sScriptSrc;
+            oHead.appendChild(oScript);
+            oScript.onload = loadedCallback;
+        };
+
+        const addRestaurantMarker = (restaurant) => {
+            const marker = new google.maps.Marker({
+                position: { lat: restaurant.lat, lng: restaurant.lng },
+                map: mapRef.current,
+            });
+        };
+
+        const getNearbyRestaurants = async(lat, lng) => {
+            getData(
+                    RESTAURANT_REST_URL +
+                    "?" +
+                    new URLSearchParams({
+                        lat: lat,
+                        lng: lng,
+                        dst: SEARCH_RADIUS,
+                    })
+                )
+                .then((data) => {
+                    console.log(data);
+                    data.map((restaurant) => {
+                        addRestaurantMarker(restaurant);
+                    });
                 })
-            )
-            .then((data) => {
-                console.log(data);
-            })
-            .catch((err) => {});
-    };
+                .catch((err) => {});
+        };
 
-    // Initialize and add the map
-    const initMap = () => {
-        // The location of Uluru
-        const hong_kong = { lat: 22.3526632, lng: 113.8475072 };
-        // The map, centered at Uluru
-        const infoWindow = new google.maps.InfoWindow();
+        // Initialize and add the map
+        const initMap = () => {
+            // The location of Uluru
+            const hong_kong = { lat: 22.3526632, lng: 113.8475072 };
+            // The map, centered at Uluru
+            const infoWindow = new google.maps.InfoWindow();
 
-        const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 17,
-            center: hong_kong,
-            mapTypeControl: false,
-            streetViewControl: false,
-            fullscreenControl: false,
-            zoomControl: false,
-        });
-        // The marker, positioned at Uluru
+            mapRef.current = new google.maps.Map(document.getElementById("map"), {
+                zoom: 17,
+                center: hong_kong,
+                mapTypeControl: false,
+                streetViewControl: false,
+                fullscreenControl: false,
+                zoomControl: false,
+            });
+            // The marker, positioned at Uluru
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
-                    };
-                    pos = { lat: 22.3127038, lng: 114.1762896 }; // Override for testing
-                    map.setCenter(pos);
-                },
-                () => {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                }
-            );
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-    };
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-            browserHasGeolocation ?
-            "Error: The Geolocation service failed." :
-            "Error: Your browser doesn't support geolocation."
-        );
-        infoWindow.open(map);
-    }
-
-    useEffect(() => {
-        loadScript(googleMapSrc, initMap);
-        getData("https://greenhub.slmaaa.work/backend/r_db/all?format=json").then(
-            (data) => {
-                console.log(data);
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        };
+                        pos = { lat: 22.3127038, lng: 114.1762896 }; // Override for testing
+                        mapRef.current.setCenter(pos);
+                    },
+                    () => {
+                        handleLocationError(true, infoWindow, mapRef.current.getCenter());
+                    }
+                );
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, mapRef.current.getCenter());
             }
-        );
-    }, []);
+        };
 
-    return html `
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(
+                browserHasGeolocation ?
+                "Error: The Geolocation service failed." :
+                "Error: Your browser doesn't support geolocation."
+            );
+            infoWindow.open(map);
+        }
+
+        useEffect(() => {
+            loadScript(googleMapSrc, initMap);
+        }, []);
+
+        return html `
     <div
       id="search-bar"
       class="field my-5 mx-4 is-overlay search-bar has-addons-centered is-flex is-align-items-center is-justify-content-center"
@@ -135,6 +142,7 @@ const Search = ({ setCurrentPage, currentPage }) => {
         id="map"
         class="map-size ${isInputSearchFocused ? "disable" : ""}"
       ></div>
+      ${isInputSearchFocused ? html` <div id="search results"></div>` : html``}
       <${NavBar} setCurrentPage=${setCurrentPage} currentPage=${currentPage} />
     </div>
   `;
