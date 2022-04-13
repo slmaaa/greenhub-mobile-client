2c1be4bdda0be8f10eb4e20d368d808cf3a033f0
slmaaa <maszelong2001@gmail.com>
2022-04-14 06:32:54 +0800

    update home.js

--- home.js
+++ home.js
@@ -10,41 +10,14 @@ import {
 import NavBar from "./navigation_bar.js";
 
 export const Home = ({ setCurrentPage, user, currentPage, balance, GCash }) => {
-        const [topPercent, setTopPercent] = useState(30);
-        const [chartConfig, setChartConfig] = useState(null);
+    const [topPercent, setTopPercent] = useState(10);
 
-        useEffect(() => {
-            const data = {
-                labels: ["Unfairwood", "TamNui", "Silver bowl"],
-                datasets: [{
-                    label: "Frequency",
-                    data: [300, 50, 100],
-                    backgroundColor: [
-                        "rgb(255, 99, 132)",
-                        "rgb(54, 162, 235)",
-                        "rgb(255, 205, 86)",
-                    ],
-                    hoverOffset: 4,
-                }, ],
-            };
-            const config = {
-                type: "doughnut",
-                data: data,
-            };
-            setChartConfig(config);
-        }, []);
+    useEffect(() => {
+        console.log(user);
+    }, []);
 
-        const chart = new Chart(document.getElementById("donut-chart"), chartConfig);
-
-        useEffect(() => {
-            console.log(user);
-            return () => {
-                chart.destroy();
-            };
-        }, []);
-
-        const generateTask = (description, reward, finishedCount, totalCount) => {
-            return html `<div class="my-4">
+    const generateTask = (description, reward, finishedCount, totalCount) => {
+        return html `<div class="my-4">
       <span class="is-size-7">> ${description}</span>
       <div
         class="is-flex is-justify-content-space-between is-align-items-flex-end"
@@ -57,14 +30,14 @@ export const Home = ({ setCurrentPage, user, currentPage, balance, GCash }) => {
         </button>
       </div>
     </div>`;
-        };
+    };
 
-        return html `
+    return html `
     <div class="hero is-flex is-flex-direction-column full-height">
       <div
-        class="px-5 pt-5 is-flex-grow-1 is-flex is-flex-direction-column is-justify-content-start"
+        class=" pt-5 px-5 is-flex-grow-1 is-flex is-flex-direction-column is-justify-content-start"
       >
-        <div class="header">
+        <div class=" header">
           <h1
             class="title ml-1 is-3 has-text-primary is-primary has-text-weight-bold"
           >
@@ -80,19 +53,51 @@ export const Home = ({ setCurrentPage, user, currentPage, balance, GCash }) => {
           </div>
         </div>
 
-        <div class="is-overflow home-body">
-          <div class="box mt-3 has-background-primary-dark">
-            <span class="has-text-white has-text-weight-bold is-size-5"
-              >You are the top ${topPercent.toString()}% ${html`<br />`} user in
-              last 30 days
-            </span>
-            <br />
-            <div>
-              <canvas id="donut-chart" />
+        <div class=" home-body overflow-y">
+          <div>
+            <div class="is-flex is-flex-direction-row overflow-x">
+              <div class="is-flex is-justify-content-center info-box-container">
+                <div class="box mt-3 has-background-primary-dark info-box">
+                  <span class="has-text-white has-text-weight-bold is-size-5"
+                    >You have save<br /><span
+                      class="is-size-1 has-text-weight-bold is-underlined"
+                      >$200</span
+                    >
+                    <br />
+                    by using G-Cash
+                  </span>
+                </div>
+              </div>
+              <div class="is-flex is-justify-content-center info-box-container">
+                <div class="box mt-3 has-background-info-dark info-box">
+                  <span class="has-text-white has-text-weight-bold is-size-5"
+                    >You are the top <br /><span
+                      class="is-size-1 has-text-weight-bold is-underlined"
+                      >${topPercent.toString()}%</span
+                    >
+                    <br />
+                    user in last 30 days
+                  </span>
+                </div>
+              </div>
+              <div
+                class="is-flex is-justify-content-center info-box-container "
+              >
+                <div class="box mt-3 has-background-warning-dark info-box">
+                  <span class="has-text-white has-text-weight-bold is-size-5"
+                    >You have save <br /><span
+                      class="is-size-1 has-text-weight-bold is-underlined"
+                      >60L</span
+                    >
+                    <br />
+                    space in the landfill
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div class="box has-background-primary-light">
+          <div class="mt-5 box has-background-primary-light">
             <div class="is-flex is-justify-content-space-between">
               <div>
                 <span class="is-size-6 has-text-weight-light"
